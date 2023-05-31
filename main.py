59aedd823d0f6ada034dad3484737e58b884c53f
Nikola Malinovic <nikolamalinovic42@gmail.com>
2023-05-31 23:30:31 +0200

    Refactor main

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from apikey import apikey
 import streamlit as st
 from app import load_index
 
-# Store API key in environment variable
-os.environ["OPENAI_API_KEY"] = apikey
 
-if not hasattr(st.session_state, 'chat'):
-    st.session_state['chat'] = []
+def initialize():
+    # Store API key in environment variable
+    os.environ["OPENAI_API_KEY"] = apikey
+    # Prepare index for querying
+    global index
+    index = load_index()
+    # Initialize global state for persistance between messages
+    if "chat" not in st.session_state:
+        st.session_state["chat"] = []
 
-index = load_index()
 
-st.title('🦾 Northprim chat bot 🤖')
-prompt_input = st.text_input("Ask me anything")
+def render_app():
+    st.title("🦾 Northprim chat bot 🤖")
+    prompt_input = st.text_input("Ask me anything")
+
+    if prompt_input:
+        response = index.query(prompt_input)
+        add_to_chat_history(prompt_input, response)
+
 
 def render_chat_history():
     container_style = "\
@@ -21,18 +31,25 @@ def render_chat_history():
         display: flex; \
         flex-direction: column-reverse;\
     "
-    chat_string = "<br>".join(st.session_state['chat'])
-    st.write(f"<div style={container_style}><p>{chat_string}</p></div>", unsafe_allow_html=True)
+    chat_string = "<br>".join(st.session_state["chat"])
+    st.write(
+        f"<div style={container_style}><p>{chat_string}</p></div>",
+        unsafe_allow_html=True,
+    )
 
-def add_to_chat_history(input, response): 
+
+def add_to_chat_history(input, response):
     user_question = f"<span style='color: green;'>User: </span><span>{input}</span>"
     bot_reponse = f"<span style='color: blue;'>Bot: </span><span>{response}</span>"
-    new_chat_value = [user_question, bot_reponse] + st.session_state['chat']
-    st.session_state['chat'] = new_chat_value
+    new_chat_value = [user_question, bot_reponse] + st.session_state["chat"]
+    st.session_state["chat"] = new_chat_value
 
-if prompt_input:
-    response = index.query(prompt_input)
-    add_to_chat_history(prompt_input, response)
-    print(st.session_state['chat'])
 
-render_chat_history()
+def main():
+    initialize()
+    render_app()
+    render_chat_history()
+
+
+if __name__ == "__main__":
+    main()
